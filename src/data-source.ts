45e1a870f78d41350a3b2b5d3e402ad093d84a8e
amirbar002 <109974156+amirbar002@users.noreply.github.com>
2023-09-27 12:37:09 +0300

    Update data-source.ts

--- src/data-source.ts
+++ src/data-source.ts
@@ -5,7 +5,7 @@ import { Recipes } from "./entity/Recipes";
 
 export const AppDataSource = new DataSource({
   type: 'mysql',
-  host: 'ec2-52-50-161-219.eu-west-1',
+  host: 'ec2-52-50-161-219.eu-west-1.compute.amazonaws.com',
   port: +5432,
   username: 'kwpaabboccyfgd',
   password:'89ab46eb9d5504d1ce0cca421d89a1557fe2a4e97bac83a2ef66978354740fab',
