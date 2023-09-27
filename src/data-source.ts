5921f5014bf075ae0c2079c6d11babf8004a89ba
amirbar002 <amirbar002@gmail.com>
2023-09-27 11:54:15 +0300

    cors

--- src/data-source.ts
+++ src/data-source.ts
@@ -5,19 +5,11 @@ import { Recipes } from "./entity/Recipes";
 
 export const AppDataSource = new DataSource({
   type: 'mysql',
-<<<<<<< HEAD
-  host: 'localhost',
-  port: +3306,
-  username: 'root',
-  password:'root',
-  database: "super",
-=======
-  host: 'ec2-52-50-161-219.eu-west-1.compute.amazonaws.com',
+  host: 'ec2-52-50-161-219.eu-west-1',
   port: +5432,
   username: 'kwpaabboccyfgd',
-  password: '89ab46eb9d5504d1ce0cca421d89a1557fe2a4e97bac83a2ef66978354740fab',
-  database: 'dqq96mo44mbgt',
->>>>>>> 22adc3984770fdce58c2bde8b46ddbc3a384857d
+  password:'89ab46eb9d5504d1ce0cca421d89a1557fe2a4e97bac83a2ef66978354740fab',
+  database: "dqq96mo44mbgt",
   synchronize: true,
   logging: false,
   entities: [Recipes],
